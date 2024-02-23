2d5047d84a3626bb7b614aff39fef8878889d213
axell.solis <axell.solis@afirma-solutions.com>
2024-02-22 19:33:01 -0600

    ref: change harpoon mapping

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,12 +2,20 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function ()
+	config = function()
 		local harpoon = require("harpoon")
 		harpoon:setup()
-		vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-		vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-		vim.keymap.set("n", "<tab>", function() harpoon:list():next() end)
-		vim.keymap.set("n", "<s-tab>", function() harpoon:list():prev() end)
-	end
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():append()
+		end)
+		vim.keymap.set("n", "<tab>", function()
+			harpoon:list():next()
+		end)
+		vim.keymap.set("n", "<s-tab>", function()
+			harpoon:list():prev()
+		end)
+	end,
 }
